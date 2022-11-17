9d0b034246811f5c0986211abb1fd0e7f282ba84
ES-12 <evgenyseleznev@gmail.com>
2022-11-17 20:12:25 +0300

    First commit

---
+++ Xcode-weather/CityCell.swift
@@ -0,0 +1,15 @@
+//
+//  CityCell.swift
+//  Xcode-weather
+//
+//  Created by user on 17.11.2022.
+//
+
+import UIKit
+
+class CityCell: UITableViewCell {
+
+    @IBOutlet weak var cityName: UILabel!
+    @IBOutlet weak var cityTemp: UILabel!
+
+}
